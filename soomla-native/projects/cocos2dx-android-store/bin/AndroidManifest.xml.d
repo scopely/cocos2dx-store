2ea9da8844d50e130dd6209a1c003f2488505644
Andrew Ross <aross@nindy.com>
2014-06-06 08:42:31 -0400

    merge with latest soomla code

---
+++ soomla-native/projects/cocos2dx-android-store/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/soomla-native/projects/cocos2dx-android-store/bin/AndroidManifest.xml : \
+/Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/soomla-native/projects/cocos2dx-android-store/AndroidManifest.xml \
---
+++ soomla-native/projects/cocos2dx-android-store/gen/R.java.d
@@ -0,0 +1,4 @@
+/Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/soomla-native/projects/cocos2dx-android-store/gen/com/soomla/R.java \
+ : /Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/submodules/android-store/SoomlaAndroidStore/res/values/soomla.xml \
+/Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/submodules/android-store/SoomlaAndroidStore/res/values/strings.xml \
+/Users/andrewross/Documents/src/cocos2d-x-2.2.2/extensions/cocos2dx-store/soomla-native/projects/cocos2dx-android-store/bin/AndroidManifest.xml \
